bdc9801b93e83f8ed82681bd31616b5134f70924
hongwah <73833818+hongwah@users.noreply.github.com>
2021-02-19 19:25:46 +0800

    register cust

---
+++ WebApplicationAssigment/pages/main/ArtShop/Customer/Register_Cust.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationAssigment.pages.main.ArtShop.Customer
+{
+    public partial class Register_Cust : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
