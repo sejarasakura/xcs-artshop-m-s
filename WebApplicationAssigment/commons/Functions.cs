44df370a37359fc919646f4c70d7afd86e7f6a25
Lim Sai Keat <saikeat.lim@mycareconcierge.com>
2021-02-27 06:53:16 +0800

    update

--- WebApplicationAssigment/commons/Functions.cs
+++ WebApplicationAssigment/commons/Functions.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 
 namespace WebApplicationAssigment.commons
 {
     public class Functions
     {
+        public static byte[] ImmageAssetToByte(String path)
+        {
+            byte[] imageBytes = null;
+            using (var webClient = new WebClient())
+            {
+                imageBytes = webClient.DownloadData("https://" + HttpContext.Current.Request.Url.Authority + path);
+            }
+            return imageBytes;
+        }
     }
 }
