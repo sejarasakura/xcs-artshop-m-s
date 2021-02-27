41d5a3883241e71caec2cc0ed7243a80d48aab2d
tudonkey521 <raymondting521@gmail.com>
2021-02-27 22:06:52 +0800

    update

--- WebApplicationAssigment/pages/main/ArtShop/ShoppingCart.aspx.cs
+++ WebApplicationAssigment/pages/main/ArtShop/ShoppingCart.aspx.cs
@@ -20,12 +20,21 @@ namespace WebApplicationAssigment.pages.main.ArtShop
 
                 total = 0;
                 Guid id = (Guid)Functions.getLoginUser().ProviderUserKey;
+                
                 IQueryable<vw_customer_cart> x = db.vw_customer_cart.Where(s => s.user_id == id);
                 vw_customer_cart[] y = x.ToArray();
                 for (int i = 0; i < x.Count(); i++)
                 {
                     total += Convert.ToDouble(y[i].price);
                 }
+
+                SqlDataSource1.SelectParameters.Remove(new Parameter("@user_id"));
+                SqlDataSource1.SelectParameters.Add("user_id", id.ToString());
+                int cart_id = db.Carts.Where(s => s.user_id == id).FirstOrDefault().id;
+                SqlDataSource1.DeleteParameters.Remove(new Parameter("@cart_id"));
+                SqlDataSource1.DeleteParameters.Add("cart_id", cart_id.ToString());
+
+
 
                 totalprice.Text ="Total price : RM" + total.ToString();
             }
